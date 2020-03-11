051f960876f7a4bc5d6dd010accb86b7609f255f
black <gjcjk123@bk.ru>
2020-03-11 22:24:42 +0300

    второе задание изменил вторую часть

--- Методы сбора и обработки данных из сети Интернет/2/парсинг_html_beautifulsoup,_mongodb.py
+++ Методы сбора и обработки данных из сети Интернет/2/парсинг_html_beautifulsoup,_mongodb.py
@@ -39,7 +39,7 @@ import time
 
 """## задаем headers"""
 
-headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/80.0.3987.87 Chrome/80.0.3987.87 Safari/537.36'}
 
 """## функция для сбора"""
 
@@ -96,53 +96,109 @@ with open('hunter.json','w') as ht:
   json.dump(hunt,ht)
 pd.read_json('hunter.json')
 
-"""## superjob.ru"""
+"""## superjob.ru
 
-from bs4 import BeautifulSoup as bs
+## функция для сбора решил подругому сделать
+"""
 
-def superjob(main_link, search_str, n_str):
-    #n_str - кол-во просматриваемых страниц
-    html = requests.get(main_link+'/vacancy/search/?keywords='+search_str+'&geo%5Bc%5D%5B0%5D=1',headers=headers).text
-    parsed_html = bs(html,'lxml')
+def parser_superjob(vacancy):
+    vacancy_date = []
+    
+    params = {
+        'keywords': vacancy, 
+        'profession_only': '1',
+        'geo[c][0]': '15', 
+        'geo[c][1]': '1', 
+        'geo[c][2]': '9', 
+        'page': ''
+    }
+    
+    headers = {
+        'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/80.0.3987.87 Chrome/80.0.3987.87 Safari/537.36'
+    }
+    
+    link = 'https://www.superjob.ru/vacancy/search/'
+       
+    html = requests.get(link, params=params, headers=headers)
+    
+    if html.ok:
+        parsed_html = bs(html.text,'lxml')
+    
+        page_block = parsed_html.find('a', {'class': 'f-test-button-1'})
+    if not page_block:
+        last_page = 1
+    else:
+        page_block = page_block.findParent()
+        last_page = int(page_block.find_all('a')[-2].getText())
+    
+    for page in range(0, last_page + 1):
+        params['page'] = page
+        html = requests.get(link, params=params, headers=headers)
+        
+        if html.ok:
+            parsed_html = bs(html.text,'html.parser')
+            vacancy_items = parsed_html.find_all('div', {'class': 'f-test-vacancy-item'})
+                        
+            for item in vacancy_items:
+                vacancy_date.append(parser_item_superjob(item))
+                
+    return vacancy_date
 
-    jobs = []
-    for i in range(n_str):
-        jobs_block = parsed_html.find('div',{'class':'_1ID8B'})
-        jobs_list = jobs_block.findChildren(recursive=False)
-        for job in jobs_list:
-            job_data={}
-            req=job.find('div',{'class':'_3syPg _1_bQo _2FJA4'})
-            if req!=None:
-                main_info = req.findChild()
-                try:
-                    job_link = main_info['href']
-                except:
-                    job_link = ''
-                job_name = main_info.getText()
-                salary = job.find('span',{'class':'_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'})
-                if not salary:
-                    salary_min=0
-                    salary_max=0
-                else:
-                    salary=salary.getText().replace(u'\xa0', u' ')
-                    salaries=salary.split('-')
-                    salary_min=salaries[0]
-                    if len(salaries)>1:
-                        salary_max=salaries[1]
-                    else:
-                        salary_max=''
-                job_data['name'] = job_name
-                job_data['salary_min'] = salary_min
-                job_data['salary_max'] = salary_max
-                job_data['link'] = job_link
-                job_data['site'] = main_link
-                jobs.append(job_data)
-        time.sleep(1)
-        next_btn_block=parsed_html.find('a',{'rel':'next'})
-        next_btn_link=next_btn_block['href']
-        html = requests.get(main_link+next_btn_link,headers=headers).text
-        parsed_html = bs(html,'lxml')
+def parser_item_superjob(item):
 
-    pprint(jobs)
+    vacancy_date = {}
+    
+    # vacancy_name
+    vacancy_name = item.find_all('a')
+    if len(vacancy_name) > 1:
+        vacancy_name = vacancy_name[-2].getText()
+    else:
+        vacancy_name = vacancy_name[0].getText()
+    vacancy_date['vacancy_name'] = vacancy_name
+    
+      
+    #salary
+    salary = item.find('span', {'class': 'f-test-text-company-item-salary'}).findChildren()
+    if not salary:
+        salary_min = None
+        salary_max = None
+        salary_currency = None
+    else:
+        salary_currency = salary[-1].getText()
+        is_check_sarary = item.find('span', {'class': 'f-test-text-company-item-salary'}).getText().replace(u'\xa0', u' ').split(' ', 1)[0]
+        if is_check_sarary == 'до' or len(salary) == 2:
+            salary_min = None
+            salary_max = int(salary[0].getText().replace(u'\xa0', u''))
+        elif is_check_sarary == 'от':
+            salary_min = int(salary[0].getText().replace(u'\xa0', u''))
+            salary_max = None
+        else:
+            salary_min = int(salary[0].getText().replace(u'\xa0', u''))
+            salary_max = int(salary[2].getText().replace(u'\xa0', u''))           
+        
+    vacancy_date['salary_min'] = salary_min
+    vacancy_date['salary_max'] = salary_max
+    vacancy_date['salary_currency'] = salary_currency
+    
+    
+    # link
+    vacancy_link = item.find_all('a')
+    
+    if len(vacancy_link) > 1:
+        vacancy_link = vacancy_link[-2]['href']
+    else:
+        vacancy_link = vacancy_link[0]['href']
+    
+    vacancy_date['vacancy_link'] = f'https://www.superjob.ru{vacancy_link }'
+    
+    # site
+    vacancy_date['site'] = 'www.superjob.ru'
+    return vacancy_date
 
-superjob('https://www.superjob.ru/','Python',1)
+"""## Преобразуем dataFrame и просмтариваем"""
+
+vacancy = 'Python'
+df = parser_superjob(vacancy)
+with open('jobs.json','w') as t:
+  json.dump(df,t)
+pd.read_json('jobs.json')
